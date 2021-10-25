de26d6cf2b476e58b737cc7646bf6350563be6a0
Imran <imran@wsegames.com>
2021-10-26 01:47:04 +0530

    initial commit

--- router/index.js
+++ router/index.js
@@ -1,9 +1,35 @@
-const NetworkSpeed = require("network-speed"); // ES5
+const FastSpeedtest = require("fast-speedtest-api");
 const express = require("express");
 const router = express.Router();
 const os = require("os");
-const testNetworkSpeed = new NetworkSpeed();
+const myPromise = fastnetApi();
+myPromise.then((res) => {
+  console.log("it returned:", res);
+  return res;
+});
 
+function fastnetApi() {
+  let speedtest = new FastSpeedtest({
+    token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm", //**** required
+    verbose: false, // default: false
+    timeout: 5000, // default: 5000
+    https: true, // default: true
+    urlCount: 5, // default: 5
+    bufferSize: 8, // default: 8
+    unit: FastSpeedtest.UNITS.Mbps, // default: Bps
+  });
+
+  return speedtest
+    .getSpeed()
+    .then((s) => {
+      console.log(`Speed: ${s} Mbps`);
+      console.log(typeof s);
+      return s;
+    })
+    .catch((e) => {
+      console.error(e.message);
+    });
+}
 router.get("/", (req, res) => {
   let details = {};
   var totalMemory = os.totalmem();
@@ -17,18 +43,8 @@ router.get("/", (req, res) => {
   details.memory = memory;
   const perc = (currentCPUUsage / total) * 100;
   details.cpu = `${perc}%`;
-  const options = {
-    hostname: "www.google.com",
-    port: 80,
-    path: "/catchers/544b09b4599c1d0200000289",
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  const fileSizeInBytes = 2000000;
-  let speed = testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
-  details.speed = speed;
+  details.speed = fastnetApi();
+  console.log(details);
   const jsonData = JSON.stringify(details);
   res.header("Access-Control-Allow-Origin", "*");
   res.send(jsonData);
